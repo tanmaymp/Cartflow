c3307127da03e9f27289810ae251b1874f5be37b
Tanmay Parulekar <tmparulekar@gmail.com>
2025-06-09 19:31:05 -0700

    data export DAG added

---
+++ airflow/dags/crtfl_export.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 0
+}
+
+EXPORT_PATH = "/opt/airflow/data_exports"
+PG_CONNECTION_STRING = os.getenv("PG_CONNECTION_STRING")
+
+def export_table_to_csv(table_name):
+    engine = create_engine(PG_CONNECTION_STRING)
+    query = f"SELECT * FROM {table_name};"
+    df = pd.read_sql(query, engine)
+    filename = os.path.join(EXPORT_PATH, f"{table_name}.csv")
+    df.to_csv(filename, index=False)
+    print(f"Exported {table_name} to {filename}")
+
+with DAG(
+    dag_id="export_marts_to_csv",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    description="Export DBT marts to CSV"
+) as dag:
+
+    export_user_orders = PythonOperator(
+        task_id="export_fct_user_orders",
+        python_callable=export_table_to_csv,
+        op_args=["fact_user_orders"]
+    )
+
+    export_user_reorders = PythonOperator(
+        task_id="export_fct_user_product_reorders",
+        python_callable=export_table_to_csv,
+        op_args=["fct_user_product_reorders"]
+    )
+
+    export_customer_orders = PythonOperator(
+        task_id="export_fct_customer_orders",
+        python_callable=export_table_to_csv,
+        op_args=["fct_customer_orders"]
+    )
+
+    export_user_orders >> export_user_reorders >> export_customer_orders
